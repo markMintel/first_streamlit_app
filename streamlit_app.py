354a4fec96e594447d60b63841a9ed54395c8915
markMintel <101613208+markMintel@users.noreply.github.com>
2023-07-25 16:15:41 -0400

    Update streamlit_app.py
    edit typos

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ s.dataframe(fruityvice_normalized)
 
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**s.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
