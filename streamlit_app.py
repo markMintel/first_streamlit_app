9ddf4ca0cf95226a529c1f48f40c28e3b9efcfd2
markMintel <101613208+markMintel@users.noreply.github.com>
2023-07-25 09:18:34 -0400

    Update streamlit_app.py
    Changed the table index to "Fruit"

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ s.text('🥑🍞 Avacado Toast')
 s.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Create a pick list to pick which friuts they want
 s.multiselect('Pick some fruits:', list(my_fruit_list.index))
