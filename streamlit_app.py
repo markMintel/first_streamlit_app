007ad970edab06fe897a0304d7a4f54f665d61f2
markMintel <101613208+markMintel@users.noreply.github.com>
2023-07-25 09:08:53 -0400

    Update streamlit_app.py
    New header for BYO smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ s.text('🥣 Omega 3 & Blueberry Oatmeal')
 s.text('🥗 Kale, SPinach & Rocket Smoothie')
 s.text('🐔 Hard-Boiled Free-Range Egg')
 s.text('🥑🍞 Avacado Toast')
+
+s.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
