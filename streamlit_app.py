67b31d3778be9b3c2c17523db58d2bf7dca6ad07
markMintel <101613208+markMintel@users.noreply.github.com>
2023-07-25 16:18:25 -0400

    Update streamlit_app.py
    Query snowflake data

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**s.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-s.text("Hello from Snowflake:")
+s.text("The fruit load list contains:")
 s.text(my_data_row)
